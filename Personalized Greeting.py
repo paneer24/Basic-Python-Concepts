f8b8a360acc8af76693ec34eda2ab835c44a51aa
Paneer Selvam <99405385+paneer24@users.noreply.github.com>
2025-03-31 09:52:35 +0530

    Update Personalized Greeting.py

--- Personalized Greeting.py
+++ Personalized Greeting.py
@@ -1,3 +1,3 @@
-first_name=input("Enter the first name: "))
-last_name=input("Enter the last name: "))
+first_name=input("Enter the first name: ")
+last_name=input("Enter the last name: ")
 print(f"Hello, {first_name} {last_name}! Welcome to Tutedude")
